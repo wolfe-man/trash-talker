16bdda97dacc194d2b0772db695d14397de1a7ca
Charles Wolfe <cwolfe@opinionlab.com>
2017-03-13 09:52:57 -0500

    sports analyst

--- src/trash_talker/util.clj
+++ src/trash_talker/util.clj
@@ -3,20 +3,20 @@
 
 (defn help-request []
   {:title "Help"
-   :output (str "The analyst knows everything. Just say, alexa, ask analyst"
-                " what is his opinion of Muhammad Ali?")
-   :reprompt-text (str "The analyst knows everything. Just say, alexa, ask analyst"
-                       " what is his opinion of Muhammad Ali?"
+   :output (str "The sports analyst knows everything. Just say, alexa, ask "
+                "sports analyst what is his opinion of Muhammad Ali?")
+   :reprompt-text (str "The sports analyst knows everything. Just say, alexa, ask "
+                       "sports analyst what is his opinion of Muhammad Ali?"
                        "Do you want to try?")
    :should-end-session false})
 
 
 (defn launch-request []
   {:title "Welcome"
-   :output (str "Please allow me to introduce you to the analyst. "
-                "The analyst knows everything. Just say, alexa, ask analyst"
-                " what is his opinion of Muhammad Ali?")
-   :reprompt-text (str "The analyst knows everything. Just say, alexa, ask analyst"
-                       " what is his opinion of Muhammad Ali?"
+   :output (str "Please allow me to introduce you to the sports analyst. "
+                "The sports analyst knows everything. Just say, alexa, ask "
+                "sports analyst what is his opinion of Muhammad Ali?")
+   :reprompt-text (str "The sports analyst knows everything. Just say, alexa, ask "
+                       "sports analyst what is his opinion of Muhammad Ali?"
                        "Do you want to try?")
    :should-end-session false})
